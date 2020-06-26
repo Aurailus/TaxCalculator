a59a21dd660638574ab03ecc4bcac8a674c8877e
Nicole Collings <100Toby1@gmail.com>
2020-06-26 16:41:19 -0700

    HTTPS Support

--- server/App.ts
+++ server/App.ts
@@ -1,5 +1,7 @@
+import fs from "fs";
 import path from "path";
 import http from "http";
+import https from "https";
 import Express from "express";
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
@@ -11,7 +13,8 @@ import CalcRouter from "./routers/CalcRouter";
 import AdminRouter from "./routers/AdminRouter";
 
 export default class App {
-	private port: number = 3000;
+	private httpPort: number = 80;
+	private httpsPort: number = 443;
 
 	private app: Express.Application = Express();
 
@@ -20,7 +23,6 @@ export default class App {
 	private adminRouter: AdminRouter = new AdminRouter(this.db, this.app);
 
 	constructor() {
-
 		this.app.use(cookieParser());
 		this.app.use(bodyParser.json());
 		this.app.use(userAgent.express());
@@ -28,14 +30,38 @@ export default class App {
 		this.app.use(fileUpload({limits: {fileSize: 2 * 1024 * 1024}, useTempFiles: true, tempFileDir: '/tmp/'}));
 		this.app.set('view engine', 'pug');
 
+		let priv: string, cert: string;
+		let enableHTTPS = true;
+		try {
+			priv = fs.readFileSync('cert/cert.key', 'utf8');
+			cert = fs.readFileSync('cert/cert.crt', 'utf8');
+		}
+		catch (e) {
+			console.log("Failed to get keys, disabling HTTPS.", e);
+			enableHTTPS = false;
+		}
+
 		this.db.init().then(async () => {
 			this.calcRouter.init();
 			this.adminRouter.init();
 			this.app.get('/', (req, res) => res.render('home'));
 			this.app.use('/public', Express.static(path.join(__dirname, "/../public")));
 
-			const server = http.createServer(this.app);
-			server.listen(this.port, () => console.log(`Listening on ${this.port}.`));
+			if (enableHTTPS) {
+				const httpServer = http.createServer((req, res) => {
+			    res.writeHead(301, { "Location": "https://" + req.headers['host']!
+			    	.replace(this.httpPort.toString(), this.httpsPort.toString()) + req.url });
+			    res.end();
+				});
+				const httpsServer = https.createServer({key: priv, cert: cert}, this.app);
+
+				httpServer.listen(this.httpPort, () => console.log(`Listening on ${this.httpPort}.`));
+				if (enableHTTPS) httpsServer.listen(this.httpsPort, () => console.log(`Listening on ${this.httpsPort}.`));
+			}
+			else {
+				const httpServer = http.createServer(this.app);
+				httpServer.listen(this.httpPort, () => console.log(`Listening on ${this.httpPort}.`));
+			}
 		});
 	}
 }
